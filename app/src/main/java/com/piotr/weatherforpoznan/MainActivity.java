ebb03fc97430c6abb7fd0bb1eab7afacb9f5f932
piotrek1543 <piotrekert90@gmail.com>
2015-09-16 23:53:13 +0200

    Updating project. Deleting toolbar and content_main.xml.

--- app/src/main/java/com/piotr/weatherforpoznan/MainActivity.java
+++ app/src/main/java/com/piotr/weatherforpoznan/MainActivity.java
@@ -2,17 +2,17 @@ package com.piotr.weatherforpoznan;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MainActivityFragment())
+                    .commit();
+        }
     }
 }
