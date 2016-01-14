a616a18b09f3fcf2a5cfb4a6aaf63548a3d72b82
piotr.ekert <piotrekert@outlook.com>
2016-01-14 19:10:26 +0100

    Creating first `Hello World` notification.

--- app/src/main/java/com/piotr/weatherforpoznan/MainActivity.java
+++ app/src/main/java/com/piotr/weatherforpoznan/MainActivity.java
@@ -1,6 +1,16 @@
 package com.piotr.weatherforpoznan;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -42,10 +52,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @AfterViews
+    protected void setWeatherNotifications() {
+        Intent intent = new Intent(this, DetailsActivity.class);
+        int requestID = (int) System.currentTimeMillis();
+        int flags = PendingIntent.FLAG_CANCEL_CURRENT;
+        PendingIntent pIntent = PendingIntent.getActivity(this, requestID, intent, flags);
+        Notification noti =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.art_clear)
+                        .setLargeIcon(drawableToBitmap(getResources()
+                                .getDrawable(R.drawable.art_clear)))
+                        .setContentTitle(getString(R.string.app_name))
+                        .setContentText("Hello World!")
+                        .setContentIntent(pIntent).build();
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(0, noti);
+
+    }
+
+    @AfterViews
     public void setMainActivityActionBar() {
         setWeatherFragments(null);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.actionbar_main);
     }
+
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if (bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    //FIXME: Reformat code, extract to methods
 }
