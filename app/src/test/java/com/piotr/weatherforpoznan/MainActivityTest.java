295eff0877fc3461e387b01d82486626540ff9ac
piotrek1543 <piotrekert90@gmail.com>
2016-04-30 23:59:44 +0200

    Working on MainActivityTest unit testing.

---
+++ app/src/test/java/com/piotr/weatherforpoznan/MainActivityTest.java
@@ -0,0 +1,23 @@
+package com.piotr.weatherforpoznan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Piotr on 30.04.2016.
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class MainActivityTest {
+
+    @Test
+    public void testOnCreateView() throws Exception {
+        MainActivity_ activity = Robolectric.setupActivity(MainActivity_.class);
+        assertTrue(true);
+    }
+}
