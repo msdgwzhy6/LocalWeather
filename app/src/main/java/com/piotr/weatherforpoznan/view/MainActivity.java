65a03228116d95883932a579261bac92a25931b3
piotrek1543 <piotrekert90@gmail.com>
2016-08-05 22:37:41 +0200

    Adding onCreateOptionsMenu and onOptionsItemSelected.

--- app/src/main/java/com/piotr/weatherforpoznan/view/MainActivity.java
+++ app/src/main/java/com/piotr/weatherforpoznan/view/MainActivity.java
@@ -3,6 +3,8 @@ package com.piotr.weatherforpoznan.view;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.piotr.weatherforpoznan.R;
@@ -64,5 +66,27 @@ public class MainActivity extends AppCompatActivity {
         //TODO: Set ACTUAL notification data on every application run
         //TODO: Notification as Service running in the background
         //FIXME: Refreshing notification data
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
